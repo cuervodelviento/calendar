0b6183a8b99a5b0dbe8f0560e77402c86742be7d
Angel <cuervodelviento@gmail.com>
2023-03-03 11:13:43 -0400

    icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Container from './components/Container'
 
 import '@fontsource/roboto'
 import './styles/global.css'
-
+import './styles/icons.css'
 function App() {
   return (
     <div className="App">
