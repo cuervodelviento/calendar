2742c656627ad1588e665027944f1cdd1efdbce3
Angel <cuervodelviento@gmail.com>
2023-03-06 12:28:29 -0400

    days header as styled components

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,20 +1,75 @@
-import { CalendarWeek, CalendarWrapper, Item, ItemDetail } from './styled'
+import { CalendarDay, CalendarWeek, CalendarWrapper, Item, ItemDetail } from './styled'
 
 const Calendar = () => {
   return (
     <CalendarWrapper>
       <CalendarWeek>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
+        <CalendarDay>
+          <a href="#dia">
+            <time dateTime="06-06-2021">
+              <span>DOM</span>
+              <br />
+              <span>6</span>
+            </time>
+          </a>
+        </CalendarDay>
+        <CalendarDay isActive>
+          <a href="#dia">
+            <time dateTime="07-06-2021">
+              <span>LUN</span>
+              <br />
+              <span>7</span>
+            </time>
+          </a>
+        </CalendarDay>
+        <CalendarDay>
+          <a href="#dia">
+            <time dateTime="08-06-2021">
+              <span>MAR</span>
+              <br />
+              <span>8</span>
+            </time>
+          </a>
+        </CalendarDay>
+        <CalendarDay>
+          <a href="#dia">
+            <time dateTime="09-06-2021">
+              <span>MIE</span>
+              <br />
+              <span>9</span>
+            </time>
+          </a>
+        </CalendarDay>
+        <CalendarDay>
+          <a href="#dia">
+            <time dateTime="10-06-2021">
+              <span>JUE</span>
+              <br />
+              <span>10</span>
+            </time>
+          </a>
+        </CalendarDay>
+        <CalendarDay>
+          <a href="#dia">
+            <time dateTime="11-06-2021">
+              <span>VIE</span>
+              <br />
+              <span>11</span>
+            </time>
+          </a>
+        </CalendarDay>
+        <CalendarDay>
+          <a href="#dia">
+            <time dateTime="12-06-2021">
+              <span>SAB</span>
+              <br />
+              <span>12</span>
+            </time>
+          </a>
+        </CalendarDay>
 
         <Item>
-          <ItemDetail>
+          <ItemDetail tabIndex={0}>
             <p>Hacer ejercicio</p>
             <p>
               <time dateTime="17:00">5</time>-<time dateTime="18:00">6 pm</time>
@@ -22,7 +77,7 @@ const Calendar = () => {
           </ItemDetail>
         </Item>
         <Item>
-          <ItemDetail>
+          <ItemDetail tabIndex={0}>
             <p>Hacer ejercicio</p>
             <p>
               <time dateTime="17:00">5</time>-<time dateTime="18:00">6 pm</time>
--- src/components/Calendar/styled.tsx
+++ src/components/Calendar/styled.tsx
@@ -11,7 +11,7 @@ export const CalendarWrapper = styled.div`
 
 export const CalendarWeek = styled.div`
   display: grid;
-  grid-template-columns: auto repeat(7, minmax(var(--minInlineSize), var(--maxInlineSize)));
+  grid-template-columns: auto repeat(6, minmax(var(--minInlineSize), var(--maxInlineSize)));
   grid-template-rows: 7rem repeat(24, minmax(var(--minBlockSize), var(--maxBlockSize)));
   min-block-size: 700px;
 `
@@ -21,6 +21,57 @@ export const Item = styled.div`
   border-inline-start: 1px solid currentColor;
   border-block-end: 1px solid currentColor;
   padding: 0.25rem;
+`
+export const CalendarDay = styled.h2<{ isActive?: boolean }>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  margin: 0;
+  position: sticky;
+  inset-block-start: 0;
+  background: var(--white);
+
+  a {
+    text-decoration: none;
+    color: currentColor;
+    outline: none;
+    :hover,
+    :focus {
+      span {
+        :nth-of-type(2) {
+          background-color: var(--gray10);
+        }
+      }
+    }
+  }
+  span {
+    :nth-of-type(1) {
+      font: var(--body1-regular);
+    }
+    :nth-of-type(2) {
+      font: var(--subtitle-subtitle1);
+      line-height: 2.75rem;
+      inline-size: 2.75rem;
+      display: inline-block;
+      border-radius: 50%;
+      ${({ isActive }) =>
+        isActive &&
+        `
+        background: var(--blue30);
+        color: var(--white);
+      `}
+    }
+  }
+  ::before {
+    content: '';
+    inline-size: 1px;
+    block-size: 2rem;
+    background: currentColor;
+    position: absolute;
+    inset-block-end: 0;
+    inset-inline-start: 0;
+  }
 `
 
 export const ItemDetail = styled.div`
